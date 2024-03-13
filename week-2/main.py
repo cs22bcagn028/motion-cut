9d088e4d76c2e58e3094b87ff2603cc265fa24f3
Priyanku Gogoi <workwithpriyanku@gmail.com>
2024-03-13 16:34:47 +0530

    week-2 assignment

--- week-2/main.py
+++ week-2/main.py
@@ -1,6 +1,12 @@
 def word_count(sentence):
-    wordsList = sentence.split(" ")
+    if(sentence == ""):
+        return 0
+    wordsList = sentence.split(" ") # this will convert the string into a list with each word as an element in the list. All because of the delimeter " " (space) 
     return len(wordsList)
 
 inputSentence = input("Enter any sentence \n")
-print(f"number of words : {word_count(inputSentence)}")
+
+if(word_count(inputSentence) == 0):
+    print("Invalid input! You must enter some string to check the working of the program :)")
+else:
+    print(f"number of words : {word_count(inputSentence)}")
